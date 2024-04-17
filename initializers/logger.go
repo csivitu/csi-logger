5a5ded7bb286c67b46fd6d20485108dbd6d25557
woaitsAryan <yoprince.ab@gmail.com>
2024-04-17 23:05:36 +0530

    chore: small logger changes

--- initializers/logger.go
+++ initializers/logger.go
@@ -12,43 +12,31 @@ import (
 var Logger *zap.SugaredLogger
 
 const (
-	DebugLevel = zapcore.DebugLevel // DebugLevel logs are disabled in production.
 	InfoLevel  = zapcore.InfoLevel  // InfoLevel logs are Debug priority logs.
 	WarnLevel  = zapcore.WarnLevel  // WarnLevel logs are more important than Info, but don't need individual human review.
 	ErrorLevel = zapcore.ErrorLevel // ErrorLevel logs are high-priority.
-	PanicLevel = zapcore.PanicLevel // PanicLevel logs a message, then panics.
-	FatalLevel = zapcore.FatalLevel // FatalLevel logs a message, then calls os.Exit(1).
 )
 
 const (
-	DebugLogFilePath = "logs/debug.log"
 	InfoLogFilePath  = "logs/info.log"
 	WarnLogFilePath  = "logs/warn.log"
 	ErrorLogFilePath = "logs/error.log"
-	PanicLogFilePath = "logs/panic.log"
-	FatalLogFilePath = "logs/fatal.log"
 )
 
 var logFiles struct {
-	DebugLogFile *os.File
 	InfoLogFile  *os.File
 	WarnLogFile  *os.File
 	ErrorLogFile *os.File
-	PanicLogFile *os.File
-	FatalLogFile *os.File
 }
 
 func AddLogger() {
 	openLogFiles()
 
-	debugCore := newCore(logFiles.DebugLogFile, DebugLevel)
 	infoCore := newCore(logFiles.InfoLogFile, InfoLevel)
 	warnCore := newCore(logFiles.WarnLogFile, WarnLevel)
 	errorCore := newCore(logFiles.ErrorLogFile, ErrorLevel)
-	panicCore := newCore(logFiles.PanicLogFile, PanicLevel)
-	fatalCore := newCore(logFiles.FatalLogFile, FatalLevel)
 
-	Logger = zap.New(zapcore.NewTee(debugCore, infoCore, warnCore, errorCore, panicCore, fatalCore)).Sugar()
+	Logger = zap.New(zapcore.NewTee(infoCore, warnCore, errorCore)).Sugar()
 }
 
 func newCore(LogFile *os.File, LoggerLevel zapcore.Level) zapcore.Core {
@@ -63,12 +51,9 @@ func newCore(LogFile *os.File, LoggerLevel zapcore.Level) zapcore.Core {
 }
 
 func openLogFiles() {
-	logFiles.DebugLogFile = openFile(DebugLogFilePath)
 	logFiles.InfoLogFile = openFile(InfoLogFilePath)
 	logFiles.WarnLogFile = openFile(WarnLogFilePath)
 	logFiles.ErrorLogFile = openFile(ErrorLogFilePath)
-	logFiles.PanicLogFile = openFile(PanicLogFilePath)
-	logFiles.FatalLogFile = openFile(FatalLogFilePath)
 }
 
 func openFile(LogFilePath string) *os.File {
@@ -80,11 +65,8 @@ func openFile(LogFilePath string) *os.File {
 }
 
 func LoggerCleanUp() {
-	logFiles.DebugLogFile.Close()
 	logFiles.InfoLogFile.Close()
 	logFiles.WarnLogFile.Close()
 	logFiles.ErrorLogFile.Close()
-	logFiles.PanicLogFile.Close()
-	logFiles.FatalLogFile.Close()
 	Logger.Sync()
 }
