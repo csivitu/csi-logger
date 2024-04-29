a4f0f7955c6bfb968f617dc65ae2f5d498dd11a4
woaitsAryan <yoprince.ab@gmail.com>
2024-04-29 23:53:10 +0530

    feat: added multi stage docker builds

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM golang:1.22-alpine
+FROM golang:1.22-alpine as build-stage
 
 WORKDIR /app
 
@@ -9,5 +9,17 @@ RUN go mod download
 COPY . .
 
 RUN CGO_ENABLED=0 GOOS=linux go build -o /main
+
+FROM gcr.io/distroless/base-debian11 AS release-stage
+
+WORKDIR /
+
+COPY .env .
+COPY ./views /views
+COPY ./static /static
+
+USER nonroot:nonroot
+
+COPY --from=build-stage /main /main
 
 CMD ["/main"]
--- initializers/logger.go
+++ initializers/logger.go
@@ -3,6 +3,7 @@ package initializers
 import (
 	"log"
 	"os"
+	"path/filepath"
 	"time"
 
 	"go.uber.org/zap"
@@ -57,6 +58,15 @@ func openLogFiles() {
 }
 
 func openFile(LogFilePath string) *os.File {
+	dir := filepath.Dir(LogFilePath)
+
+	if _, err := os.Stat(dir); os.IsNotExist(err) {
+		err := os.MkdirAll(dir, 0755)
+		if err != nil {
+			log.Printf("Failed to create directory: " + err.Error())
+		}
+	}
+
 	logFile, err := os.OpenFile(LogFilePath, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
 	if err != nil {
 		log.Printf("Failed to open log file: " + err.Error())
